5ede30f6b16f064e390574107a31374472e7c364
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-11-09 14:27:23 +0100

    AWS setup

--- variables.tf
+++ variables.tf
@@ -1,48 +1,42 @@
-variable "do_token" {
-  description = "Your DigitalOcean API key"
+## AWS credentials
+
+variable "aws_access_key" {
+  description = "AWS Access key"
 }
 
-variable "docker_swarm_domain" {
-  description = "Name of the DNS domain for the swarm"
-  default = "nemerosa.net"
+variable "aws_secret_key" {
+  description = "AWS Secret key"
 }
 
-variable "docker_swarm_domain_name" {
-  description = "Name of the swarm in the DNS domain"
-  default = "swarm"
+variable "aws_region" {
+  description = "AWS Region"
 }
 
-variable "do_region" {
-  description = "DigitalOcean Region"
-  default = "fra1"
-}
+## Swarm configuration
 
-variable "do_agent_size" {
-  description = "Agent Droplet Size"
-  default = "2GB"
-}
-
-variable "do_ssh_key_public" {
-  description = "Path to the SSH public key"
-  default = "./do-key.pub"
-}
-
-variable "do_ssh_key_private" {
-  description = "Path to the SSH private key"
-  default = "./do-key"
-}
-
-variable "do_swarm_name" {
+variable "swarm_name" {
   description = "Name of the cluster, used also for networking"
   default = "swarm"
 }
 
-variable "do_swarm_master_count" {
+variable "swarm_master_count" {
   description = "Number of master nodes."
   default = "1"
 }
 
-variable "do_swarm_agent_count" {
+variable "swarm_agent_count" {
   description = "Number of agents to deploy"
   default = "1"
+}
+
+## DNS setup
+
+variable "dns_domain" {
+  description = "Name of the DNS domain for the swarm"
+  default = "nemerosa.net"
+}
+
+variable "dns_entry" {
+  description = "Name of the swarm in the DNS domain"
+  default = "swarm"
 }
