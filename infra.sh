00bd86df55772a28eb4fe8f9c7c031ee30e81bf2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-11-19 18:34:12 +0100

    Sample Ontrack declaration

--- infra.sh
+++ infra.sh
@@ -6,3 +6,11 @@ docker service create \
   --constraint=node.role==manager \
   --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   manomarks/visualizer
+
+sudo mkdir -p /mnt/storage/ontrack &&
+    docker service create \
+        --name=ontrack \
+        --publish=8082:8080/tcp \
+        --mount=type=bind,src=/dev/urandom,dst=/dev/random \
+        --mount=type=bind,src=/mnt/storage/ontrack,dst=/var/ontrack/data \
+        nemerosa/ontrack:2.26.4
